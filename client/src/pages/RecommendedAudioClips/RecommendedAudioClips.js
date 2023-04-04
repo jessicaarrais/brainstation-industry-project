dcf6638c453525d3f97a0538e26712634d50a035
Jessica Arrais <jessicalarrais@gmail.com>
2023-04-04 07:32:26 -0400

    change video to audio tag

--- client/src/pages/RecommendedAudioClips/RecommendedAudioClips.js
+++ client/src/pages/RecommendedAudioClips/RecommendedAudioClips.js
@@ -15,33 +15,33 @@ export default function RecommendedAudioClips() {
         loading="lazy"
       ></iframe> */}
 
-      <video controls autoPlay name="media">
+      <audio controls autoPlay name="media">
         <source
           src="https://p.scdn.co/mp3-preview/2f37da1d4221f40b9d1a98cd191f4d6f1646ad17"
           type="audio/mpeg"
         />
-      </video>
+      </audio>
 
-      <video controls autoPlay name="media">
+      <audio controls autoPlay name="media">
         <source
           src="https://p.scdn.co/mp3-preview/671c90202a45fcb769e818eaa8fe078eb9d2bc5f"
           type="audio/mpeg"
         />
-      </video>
+      </audio>
 
-      <video controls autoPlay name="media">
+      <audio controls autoPlay name="media">
         <source
           src="https://p.scdn.co/mp3-preview/94afe36cf22748cd4d34e7bd259861fa340ec369"
           type="audio/mpeg"
         />
-      </video>
+      </audio>
 
-      <video controls autoPlay name="media">
+      <audio controls autoPlay name="media">
         <source
           src="https://p.scdn.co/mp3-preview/57e22f139de2b3b0035d5c59f8f53a620c8948c7"
           type="audio/mpeg"
         />
-      </video>
+      </audio>
     </main>
   );
 }
